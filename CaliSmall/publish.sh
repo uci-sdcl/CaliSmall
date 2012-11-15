d9db6455d650a941bdbfc930d09906c433b3dcd0
mb <michele.bonazza@gmail.com>
2012-11-14 17:26:52 -0800

    changed for new index.html

--- CaliSmall/publish.sh
+++ CaliSmall/publish.sh
@@ -3,8 +3,12 @@
 # To be called AFTER the Javadoc has been generated.
 # by Michele Bonazza
 GH_PAGES_FOLDER=/Users/michelebonazza/prove/CaliSmall
+CALISMALL_HELPER_PAGES_FOLDER=/Users/michelebonazza/prove/CaliSmallPages
 
 cd $GH_PAGES_FOLDER
+
+mv index.html javadoc.html
+cp $CALISMALL_HELPER_PAGES_FOLDER/* .
 
 git add . || exit $?
 git commit -a -m "generated javadoc"
